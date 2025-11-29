3fe45277b35c615f9179f96e5336fe9ea0a9019c
Jarki <yaroslav.ivashko@gmail.com>
2025-11-29 21:02:55 +0200

    chore: formatting; chore: remove unnecessary files

--- src/logger_config.py
+++ src/logger_config.py
@@ -1,14 +1,14 @@
 import loguru
 
 
-def setup_logger():
+def setup_logger() -> None:
     loguru.logger.remove()
     loguru.logger.add(
-        sink=lambda msg: print(msg, end=''),
+        sink=lambda msg: print(msg, end=""),
         format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> - "
-               "[<level>{level: <8}</level>] "
-               "(<cyan>{name}</cyan>, <cyan>{line}</cyan>) - "
-               "<level>{message}</level>",
+        "[<level>{level: <8}</level>] "
+        "(<cyan>{name}</cyan>, <cyan>{line}</cyan>) - "
+        "<level>{message}</level>",
         level="DEBUG",
         colorize=True,
     )
